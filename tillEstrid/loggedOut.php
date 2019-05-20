a3b91f1436cf0d4a12fd76855fce58d09c6c7160
LisalinaGM <50692491+LisalinaGM@users.noreply.github.com>
2019-05-20 09:13:17 +0200

    Update loggedOut.php

--- tillEstrid/loggedOut.php
+++ tillEstrid/loggedOut.php
@@ -25,7 +25,7 @@ if(session_destroy())
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
       <aside id="authorInfo"> 
         <h2>You are now logged out!</h2>
-		<p>Welcome back!</p>
+		<p>Welcome back another time!</p>
 	  </aside>
     </section>
     <section id="sidebar"> 
