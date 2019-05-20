c466c761697d784523ff5638715fb24c210323b8
LisalinaGM <50692491+LisalinaGM@users.noreply.github.com>
2019-05-20 09:15:17 +0200

    Update basilico.php

--- tillEstrid/basilico.php
+++ tillEstrid/basilico.php
@@ -6,35 +6,23 @@ include('processes/session.php');
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>BlogPost template by Adobe Dreamweaver CC</title>
+<title>Basilico</title>
 <link href="assets/css/start.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 <div id="mainwrapper">
   <header> 
-    <!--**************************************************************************
-    Header starts here. It contains Logo and 3 navigation links. 
-    ****************************************************************************-->
-    <div id="logo"><!-- <img src="logoImage.png" alt="sample logo"> --><!-- Company Logo text -->Logo</div>
+    <div id="logo"><!-- <img src="logoImage.png" alt="sample logo"> -->RestaurantTips</div>
     <nav> <a href="register.php" title="Link">Sign up</a> <a href="login.php" title="Link">Log in</a><a href="#" title="Link">Log out</a> </nav>
   </header>
-  <div id="content">
-    <div class="notOnDesktop"> 
-      <!-- This search box is displayed only in mobile and tablet laouts and not in desktop layouts -->
-      <input type="text" placeholder="Search">
-    </div>
     <section id="mainContent"> 
-      <!--************************************************************************
-    Main Blog content starts here
-    ****************************************************************************-->
       <h1><!-- Blog title -->Basilico</h1>
       <h3><!-- Tagline -->SOME AWESOME HERO TAGLINE</h3>
       <div id="bannerImage"><img src="assets/img/startbild.jpg" alt=""/></div>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
       <aside id="authorInfo"> 
-        <!-- The author information is contained here -->
         <h2>Tips</h2>
 					<?php
 				$uname = "dbtrain_1036";
@@ -63,9 +51,6 @@ include('processes/session.php');
       </aside>
     </section>
     <section id="sidebar"> 
-      <!--************************************************************************
-    Sidebar starts here. It contains a searchbox, sample ad image and 6 links
-    ****************************************************************************-->
       <input type="text" placeholder="Search">
       <div id="adimage"><img src="assets/img/litenbannan.png" alt=""/></div>
       <nav>
@@ -75,7 +60,7 @@ include('processes/session.php');
           <li><a href="frenchi.php" title="Link">Frenchi</a></li>
           <li><a href="#" title="Link">Iberico</a></li>
           <li><a href="#" title="Link">Aaltos</a></li>
-          <li><a href="#" title="Link">Contact us here</a></li>
+          <li><a href="start.php" title="Link">Home</a></li>
         </ul>
       </nav>
     </section>
