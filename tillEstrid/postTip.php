608b718699146f96802d58e52732ed3c909728bb
LisalinaGM <50692491+LisalinaGM@users.noreply.github.com>
2019-05-20 10:42:15 +0200

    Update postTip.php

--- tillEstrid/postTip.php
+++ tillEstrid/postTip.php
@@ -14,25 +14,12 @@ include('processes/session.php');
 <body>
 <div id="mainwrapper">
   <header> 
-    <!--**************************************************************************
-    Header starts here. It contains Logo and 3 navigation links. 
-    ****************************************************************************-->
     <div id="logo"><!-- <img src="logoImage.png" alt="sample logo"> --><!-- Company Logo text -->Logo</div>
     <nav> <a href="register.php" title="Link">Sign up</a> <a href="login.php" title="Link">Log in</a><a href="loggedOut.php" title="Link">Log out</a> </nav>
   </header>
   <div id="content">
-    <div class="notOnDesktop"> 
-      <!-- This search box is displayed only in mobile and tablet laouts and not in desktop layouts -->
-      <input type="text" placeholder="Search">
-    </div>
     <section id="mainContent"> 
-      <!--************************************************************************
-    Main Blog content starts here
-    ****************************************************************************-->
-      <h1><!-- Blog title -->Post Title</h1>
-      <h3><!-- Tagline -->SOME AWESOME HERO TAGLINE</h3>
-      <div id="bannerImage"><img src="assets/img/startbild.jpg" alt=""/></div>
-		<h3>Write your tip to other users here!</h3>
+      <h1>Write your tip to other users here!</h1>
 		<p> Fill in the title of your tip, your email-address, select what restaurant the tip concerns and then write your thoughts down.
 			Once you click on the submit-button your tip will be posted at the side of the restaurant you have chosen.</p>
 				<form name = "inputForm" action="processes/tipProcess.php" onsubmit = "return validateForm()" method = "post">
@@ -53,9 +40,6 @@ include('processes/session.php');
 				</form>
     </section>
     <section id="sidebar"> 
-      <!--************************************************************************
-    Sidebar starts here. It contains a searchbox, sample ad image and 6 links
-    ****************************************************************************-->
       <input type="text" placeholder="Search">
       <div id="adimage"><img src="assets/img/litenbannan.png" alt=""/></div>
       <nav>
@@ -65,12 +49,12 @@ include('processes/session.php');
           <li><a href="frenchi.php" title="Link">Frenchi</a></li>
           <li><a href="#" title="Link">Iberico</a></li>
           <li><a href="#" title="Link">Aaltos</a></li>
-          <li><a href="#" title="Link">Contact us here</a></li>
+          <li><a href="start.php" title="Link">Home</a></li>
         </ul>
       </nav>
     </section>
   </div>
-  <div id="footerbar"><!-- Small footerbar at the bottom --> copyright @ Group 2. All rights reserved</div>
+  <div id="footerbar">copyright @ Group 2. All rights reserved</div>
 </div>
 </body>
 </html>
