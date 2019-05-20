224c76ad45cec8dfb0a03a1ec92e91d1f2830451
LisalinaGM <50692491+LisalinaGM@users.noreply.github.com>
2019-05-20 09:08:04 +0200

    Add files via upload

---
+++ tillEstrid/loggedOut.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Now logged out</title>
+<link href="assets/css/start.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div id="mainwrapper">
+  <header> 
+    <div id="logo">RestaurantTips</div>
+    <nav> <a href="register.php" title="Link">Sign up</a> <a href="login.php" title="Link">Log in</a><a href="#" title="Link">Log out</a> </nav>
+  </header>
+  <div id="content">
+    <section id="mainContent"> 
+      <h1>Now logged out</h1>
+      <div id="bannerImage"><img src="assets/img/startbild.jpg" alt=""/></div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+      <aside id="authorInfo"> 
+        <h2>You are now logged out!</h2>
+		<p>Welcome back!</p>
+	  </aside>
+    </section>
+    <section id="sidebar"> 
+      <input type="text" placeholder="Search">
+      <div id="adimage"><img src="assets/img/litenbannan.png" alt=""/></div>
+      <nav>
+        <ul>
+          <li><a href="postTip.php" title="Link">Post a tip</a></li>
+          <li><a href="basilico.php" title="Link">Basilico</a></li>
+          <li><a href="frenchi.php" title="Link">Frenchi</a></li>
+          <li><a href="#" title="Link">Iberico</a></li>
+          <li><a href="#" title="Link">Aaltos</a></li>
+          <li><a href="start.php" title="Link">Home</a></li>
+        </ul>
+      </nav>
+  </div>
+  <div id="footerbar">copyright @ Group 2. All rights reserved</div>
+</div>
+</body>
+</html>
