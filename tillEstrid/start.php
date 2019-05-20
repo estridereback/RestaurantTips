00205805d7cbcfb9a64ae942a8444c111b989d93
estridereback <50692632+estridereback@users.noreply.github.com>
2019-05-20 10:34:35 +0200

    Update start.php

--- tillEstrid/start.php
+++ tillEstrid/start.php
@@ -3,18 +3,18 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>BlogPost template by Adobe Dreamweaver CC</title>
+<title></title>
 <link href="assets/css/start.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 <div id="mainwrapper">
   <header> 
-    <!--**************************************************************************
+    <!--
     Header starts here. It contains Logo and 3 navigation links. 
-    ****************************************************************************-->
-    <div id="logo"><!-- <img src="logoImage.png" alt="sample logo"> --><!-- Company Logo text -->Logo</div>
-    <nav> <a href="register.php" title="Link">Sign up</a> <a href="login.php" title="Link">Log in</a><a href="loggedOut.php" title="Link">Log out</a> </nav>
+    -->
+    <div id="logo"> <img src="assets/img/loggafärg.png" alt="sample logo"<!-- Company Logo text --></div>
+    <nav> <a href="register.php" title="Link">Sign up</a> <a href="login.php" title="Link">Log in</a><a href="#" title="Link">Log out</a> </nav>
   </header>
   <div id="content">
     <div class="notOnDesktop"> 
@@ -22,26 +22,26 @@
       <input type="text" placeholder="Search">
     </div>
     <section id="mainContent"> 
-      <!--************************************************************************
+      <!--
     Main Blog content starts here
-    ****************************************************************************-->
-      <h1><!-- Blog title -->Post Title</h1>
-      <h3><!-- Tagline -->SOME AWESOME HERO TAGLINE</h3>
-      <div id="bannerImage"><img src="assets/img/startbild.jpg" alt=""/></div>
+    -->
+      <h1><!-- Blog title -->News</h1>
+      <h3><!-- Tagline -->lASTEST COCKTAIL BARS IN UPPASA</h3>
+      <div id="bannerImage"><img src="assets/img/nyabarer.jpg" alt=""/></div>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
       <aside id="authorInfo"> 
         <!-- The author information is contained here -->
-        <h2>Author Name</h2>
+        <h2>OUR BEST TIPS FOR THIS MONTH</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       </aside>
     </section>
     <section id="sidebar"> 
-      <!--************************************************************************
+      <!--
     Sidebar starts here. It contains a searchbox, sample ad image and 6 links
-    ****************************************************************************-->
+    -->
       <input type="text" placeholder="Search">
-      <div id="adimage"><img src="assets/img/litenbannan.png" alt=""/></div>
+      <div id="adimage"><img src="assets/img/trensstallen.jpg" alt=""/></div>
       <nav>
         <ul>
           <li><a href="postTip.php" title="Link">Post a tip</a></li>
@@ -54,15 +54,15 @@
       </nav>
     </section>
     <footer> 
-      <!--************************************************************************
+      <!--
     Footer starts here
-    ****************************************************************************-->
+    -->
       <article>
-        <h3>Footer1</h3>
+        <h3>VÅRA SAMMARBETEN </h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </article>
       <article>
-        <h3>Footer2</h3>
+        <h3>JOBBA MED OSS</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </article>
     </footer>
